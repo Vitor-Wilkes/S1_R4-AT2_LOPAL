eb6a30d122c3c76e2accb79aef746027e9d9ae5c
I1HSA <vitorwilkes99@gmail.com>
2026-04-13 09:12:01 -0300

    Foi feito o script base da atividade e adicionado o flowgorithm.

---
+++ script.js
@@ -0,0 +1,15 @@
+let number = Number(prompt("Insira seu número:"));
+
+let resul = number % 2
+
+if (isNaN(resul)) {
+    alert("Você não digitou um número.")
+}
+else {
+    if (resul == 0) {
+        alert("Seu número é par.")
+    }
+    else {
+        alert("Seu número é ímpar.")
+    }
+}
