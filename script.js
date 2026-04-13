3f44407af4cbb4596b8dfcc896928df7883198c7
I1HSA <vitorwilkes99@gmail.com>
2026-04-13 11:29:44 -0300

    Comentários adicionados.

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-let number = Number(prompt("Insira seu número:"));
+let number = Number(prompt("Insira seu número:")); //Essa variável guarda o número a ser verificado
 
-let resul = number % 2
+let resul = number % 2 //Essa verifica o resto do número divido por 2
 
-if (isNaN(resul)) {
+if (isNaN(resul)) { //Verifica se a variável número é um número, se não for ele barra o usuario
     alert("Você não digitou um número.")
 }
-else {
-    if (resul == 0) {
+else { //Se for, ele contina o codigo
+    if (resul == 0) { // Se o resto for 0, ele informa que o número é par
         alert("Seu número é par")
     }
-    else {
+    else { // Se não for, ele informa que é ímpar
         alert("Seu número é ímpar")
     }
 }
