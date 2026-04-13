56932d1872f1c76be9474b35321f8e220788e5fc
I1HSA <vitorwilkes99@gmail.com>
2026-04-13 11:33:38 -0300

    Comentários adicionados.

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-let number = Number(prompt("Insira o valor da compra:"));
+let number = Number(prompt("Insira o valor da compra:")); //Guarda o valor da compra
 
-let resul;
+let resul; //Cria a variável resul
 
-let desc = number * 0.02
+let desc = number * 0.02 //Essa variável guarda o desconto da compra 
 
-if (isNaN(number)) {
+if (isNaN(number)) { // Verifica se a variável number é um número, se não for ele barra o usuario
     alert("Você não digitou um número.")
 }
-else {
-    if (number >= 150) {
+else { // Se for, ele verifica se é maior/igual a 150
+    if (number >= 150) { //Se for ele dará desconto e informa o valor final
         resul = number - desc;
         alert(`O valor do desconto é de ${desc.toFixed(2)} reais, portanto o valor final é de ${resul} reais.`)
     }
-    else {
+    else { //Se não, ele dará cashback e informa o valor final
         alert(`O valor do cashback é de ${desc.toFixed(2)} reais, portanto o valor total é de ${number} reais`)
     }
 }
