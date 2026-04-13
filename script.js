d59a88ddafdd6e4690a5f1331a09db5f7bc39251
I1HSA <vitorwilkes99@gmail.com>
2026-04-13 11:35:29 -0300

    Comentários adicionados.

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-let number = Number(prompt("Insira sua altura:"));
+let number = Number(prompt("Insira sua altura:")); //Guarda o valor da altura
 
-if (isNaN(number)) {
+if (isNaN(number)) { //Verifica se a variável number é um número, se não for ele barra
     alert("Você não digitou um número.")
 }
-else {
-    if (number >= 1.5) {
+else { //Se for, ele verifica se a altura é maior/igual que 1.5 metros
+    if (number >= 1.5) { //Se for ele informa que é permitido entrar
         alert("Pode entrar no brinquedo.")
     }
-    else {
+    else { //Se não, ele nega o acesso
         alert("Acesso negado.")
     }
 }
