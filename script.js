6d0261409dae2202b9e6ef137eeb8307fb366867
I1HSA <vitorwilkes99@gmail.com>
2026-04-13 11:26:44 -0300

    Comentei o código.

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-let senha = prompt("Insira a senha");
-let senha2 = prompt("Insira novamente a senha:");
+let senha = prompt("Insira a senha"); //Essa variável guarda a senha 1
+let senha2 = prompt("Insira novamente a senha:"); // Essa variável guarda a senha 2
 
-if(senha == senha2) {
+if(senha == senha2) { // Se a senha 1 for igual a senha 2 ele permitira o acesso
     alert("Acesso permitido.")
 }
-else {
+else { //Se não for, ele negara
     alert("Acesso negado.")
 }
